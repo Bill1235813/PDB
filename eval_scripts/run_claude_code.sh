3a6da406682c610484e8d5c9a31178696895afde
Tony <qiutongy@usc.edu>
2025-11-13 11:27:43 -0800

    fixed memory problem

--- eval_scripts/run_claude_code.sh
+++ eval_scripts/run_claude_code.sh
@@ -3,7 +3,7 @@ python main.py \
   --model_name claude-code-agent \
   --use_claude_code \
   --debug_mode minimal \
-  --input_file /home/zhuwangz/miaosenchai/rescue_code_bench/eval_scripts/sampled_data_t.json \
+  --input_file /home/zhuwangz/miaosenchai/rescue_code_bench/eval_scripts/sampled_data.json \
   --max_iter 3 \
   --claude_timeout 300 \
   --log_prefix unit-claude_code_bigcodebench \
